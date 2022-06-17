4527352c87544a8563296005e1ecccf3323f29b7
jjinichoi <genie9105@gmail.com>
2022-06-17 15:45:44 +0900

    middleware 이해완료

--- src/server.js
+++ src/server.js
@@ -6,17 +6,23 @@ const app = express();
 //express application(app이 아닌 다른 이름으로 해도되지만 관습이기때문에 되도록 app으로)
 
 //application설정
+
+// loggerMiddleware는 app 전체에서 사용되고 있고 request에 관한 모든 정보를 가지고 있는 request object를 이용해서 method와 url을 console.log해주는 역할
 const loggerMiddleware = (req, res, next) => {
   console.log(`${req.method} ${req.url}`); //어떤 method가 어느 URL로 향하는지
   next();
 };
+
+//privateMiddleware는 url정보를 받아서 /protected와 같은 걸 확인하면 중간에 개입해서 다음 함수를 호출하는 걸 막고 url이 /protected가 아니라면 다음 함수 호출
 const privateMiddleware = (req, res, next) => {
   const url = req.url;
   if (url === "/protected") {
-    return res.send("<h1>Nat Allowed</h1>");
+    return res.send("<h1>Not Allowed</h1>");
   }
+  console.log("Allowed, you may continue.");
   next();
 };
+
 const handlehome = (req, res) => {
   return res.send("hi");
 };
